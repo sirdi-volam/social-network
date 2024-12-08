3509777edd342e1172c9fc65560f3a95b129b4fe
sirdi <sirdi-volam@gmail.com>
2024-12-08 06:04:41 +0300

    add sass, reset.css, normalize.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './src/styles/index.css'
+import './src/styles/reset.css'
+import './src/styles/normalize.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
